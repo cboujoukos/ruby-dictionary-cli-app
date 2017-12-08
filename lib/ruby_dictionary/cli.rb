94220bc16eaadf7e2a1269cc3e02a26a14873612
Chris Boujoukos <github email address>
2017-12-08 13:23:17 -0500

    add basic CLI controls

--- lib/ruby_dictionary/cli.rb
+++ lib/ruby_dictionary/cli.rb
@@ -4,6 +4,17 @@ class RubyDictionary::CLI
 
   def call
     puts "Welcome to the Ruby Dictionary"
+    list_data_types
+    menu
+  end
+
+  def list_data_types
+    data_types = ["Booleans", "Symbols", "Numbers", "Arrays", "Hashes"]
+    puts data_types
+  end
+
+  def menu
+    puts "Which data type would you like to explore?"
   end
 
 end
