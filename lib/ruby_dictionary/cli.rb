511a72c0b34050006b99cfcac10691646bb88178
Chris Boujoukos <github email address>
2017-12-08 13:35:08 -0500

    stubbed out menu interface

--- lib/ruby_dictionary/cli.rb
+++ lib/ruby_dictionary/cli.rb
@@ -3,18 +3,36 @@
 class RubyDictionary::CLI
 
   def call
-    puts "Welcome to the Ruby Dictionary"
     list_data_types
     menu
   end
 
   def list_data_types
+    puts "Welcome to the Ruby Dictionary"
     data_types = ["Booleans", "Symbols", "Numbers", "Arrays", "Hashes"]
     puts data_types
   end
 
   def menu
-    puts "Which data type would you like to explore?"
+    puts "Enter the data type you like to explore, or type exit."
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "booleans"
+        puts "You are now in the Boolean menu"
+      when "symbols"
+        puts "You are now in the Symbol menu"
+      when "numbers"
+        puts "You are now in the Number menu"
+      when "arrays"
+        puts "You are now in the Array menu"
+      when "hashes"
+        puts "You are now in the Hash menu"
+      else
+        puts "I'm sorry, I didn't get that. Please enter a data type or type exit."
+      end
+    end
   end
 
 end
